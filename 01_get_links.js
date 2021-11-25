2c0cb409ce9a50cd5ebc5ef70b7c8e28f1ef44fd
Alan W. Smith <alan@alanwsmith.com>
2021-11-25 13:17:57 -0500

    Setup config switch and put in some report

--- 01_get_links.js
+++ 01_get_links.js
@@ -1,4 +1,3 @@
-const grimoireDir = 'test_files'
 const fs = require('fs')
 const path = require('path')
 const matter = require('gray-matter')
@@ -7,31 +6,68 @@ const regex = /http:\/\/.*?(?=[") \]])/g
 
 const theLinks = []
 
-fs.readdirSync(grimoireDir).forEach((filename) => {
+const config = {
+  dev: {
+    sourceDir: 'test_files',
+    outputFile: 'data/01_test_list_of_files.tsv',
+  },
+  prod: {
+    sourceDir: '/Users/alans/Dropbox/grimoire',
+    outputFile: 'data/01_list_of_files.tsv',
+  },
+}
+
+const env = 'dev'
+
+const reportDetails = {
+  totalFileCount: 0,
+  siteFileCount: 0,
+  linkCount: 0,
+}
+
+fs.readdirSync(config[env].sourceDir).forEach((filename) => {
+  ////////////////////////////////////////////////////////
+  // Only look at .txt files which are the actual content
   if (filename.match(/\.txt$/)) {
-    const filePath = path.join(grimoireDir, filename)
+    const filePath = path.join(config[env].sourceDir, filename)
     console.log(filePath)
+    reportDetails.totalFileCount += 1
+
+    ///////////////////////////////////////////////////////
+    // Try to get the front matter
     let matterObject = matter.read(filePath)
-    // only process files with an id (which is requried
-    // to push the file to the site)
-    if (matterObject.data.id) {
-      console.log(`  ${matterObject.data.id}`)
-      const matches = matterObject.content.match(regex).forEach((link) => {
-        console.log(link)
-        const details = [
-          matterObject.data.id,
-          matterObject.data.date,
-          filename,
-          link,
-        ]
-        theLinks.push(details)
-      })
+
+    ///////////////////////////////////////////////////////
+    // Only process files that have frontmatter (i.e. data)
+    if (matterObject.data) {
+      /////////////////////////////////////////////////////
+      // Only process files that have an id (which are the
+      // ones that go to the site
+      if (matterObject.data.id) {
+        console.log(`-- ${matterObject.data.id}`)
+        reportDetails.siteFileCount += 1
+
+        ///////////////////////////////////////////////////
+        // Find all the links via the regex match, and push
+        // the details onto the main array
+        const matches = matterObject.content.match(regex).forEach((link) => {
+          console.log(link)
+          reportDetails.linkCount += 1
+          const details = [
+            matterObject.data.id,
+            matterObject.data.date,
+            filename,
+            link,
+          ]
+          theLinks.push(details)
+        })
+      }
     }
   }
 })
 
 fs.writeFile(
-  'data/01_list_of_links.tsv',
+  config[env].outputFile,
   theLinks
     .map((linkDetails) => {
       return linkDetails.join('\t')
@@ -44,3 +80,5 @@ fs.writeFile(
     }
   }
 )
+
+console.log(reportDetails)
